d08f3f015e3e0c6f6c0405ba3520eeeca4000838
Ibrahim799-ui <148182841+Ibrahim799-ui@users.noreply.github.com>
2023-11-20 13:08:09 +0100

    Add files via upload

---
+++ subjects.js
@@ -0,0 +1,94 @@
+let subjects = [
+    {
+        "id":101,
+        "image":"img2/math-removebg-preview.png",
+        "subjectname":"Mathematics",
+        "location":"China",
+        "price":40,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":102,
+        "image":"img2/eng-removebg-preview.png",
+        "subjectname":"English",
+        "location":"United Kingdom",
+        "price":20,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":103,
+        "image":"img2/bio.png",
+        "subjectname":"Biology",
+        "location":"Korea",
+        "price":70,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":104,
+        "image":"img2/chem.png",
+        "subjectname":"Chemistry",
+        "location":"Saudi Arabia",
+        "price":30,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":105,
+        "image":"img2/mus-removebg-preview.png",
+        "subjectname":"Music",
+        "location":"Dubia",
+        "price":120,
+        "numberofspaces": 5,
+       
+    },
+    {
+        "id":106,
+        "image":"img2/agric.png",
+        "subjectname":"Agriculture",
+        "location":"Russia",
+        "price":110,
+        "numberofspaces": 5,
+        "rating": 2
+    },
+    {
+        "id":107,
+        "image":"img2/comp-removebg-preview (3).png",
+        "subjectname":"Computer",
+        "location":"Ukraine",
+        "price":90,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":108,
+        "image":"img2/art-removebg-preview.png",
+        "subjectname":"Fine Arts",
+        "location":"Malta",
+        "price":100,
+        "numberofspaces": 5,
+       
+    },
+    {
+        "id":109,
+        "image":"img2/phy-removebg-preview (1).png",
+        "subjectname":"Physics",
+        "location":"Kenya",
+        "price":120,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":110,
+        "image":"img2/his-removebg-preview (1).png",
+        "subjectname":"History",
+        "location":"Nigeria",
+        "price":140,
+        "numberofspaces": 5,
+        
+    }
+
+   
+  ]
